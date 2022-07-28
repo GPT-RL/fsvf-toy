d8ebcaa7fded4f3d7ecb2a31f697e3d6599b4275
Ethan Brooks <ethanabrooks@gmail.com>
2022-07-28 14:15:34 -0400

    add pre-commit

--- flake.nix
+++ flake.nix
@@ -42,7 +42,7 @@
       all = p: runtime p ++ dev p;
     in {
       devShell = pkgs.mkShell {
-        buildInputs = all python.pkgs ++ [pkgs.pre-commit];
+        buildInputs = [pkgs.pre-commit] ++ all python.pkgs;
         shellHook = ''
           export pythonbreakpoint=ipdb.set_trace
         '';
