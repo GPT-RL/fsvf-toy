2ca0b0a12cb07f98daf390de1330d648bf6fb23a
Ethan Brooks <ethanabrooks@gmail.com>
2022-07-28 13:10:06 -0400

    upgrade tensorflow

--- flake.nix
+++ flake.nix
@@ -12,49 +12,52 @@
     utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {
         inherit system;
-        overlays = [
-          (final: prev: {
-            python39 = prev.python39.override {
-              packageOverrides = pyfinal: pyprev: {
-                tensorflow-datasets = pyprev.tensorflow-datasets.overridePythonAttrs (old: rec {
-                  version = "4.6.0";
-                  src = prev.fetchFromGitHub {
-                    owner = "tensorflow";
-                    repo = "datasets";
-                    rev = "v${version}";
-                    sha256 = "sha256-OZpaY/6BMISq5IeDXyuyu5L/yG+DwlFliw4BsipPOLg=";
-                  };
-                });
-              };
-            };
-          })
-        ];
       };
-      run-dependencies = p:
+      python = let
+        packageOverrides = pyfinal: pyprev: {
+          etils = import ./etils.nix {
+            inherit pkgs pyfinal pyprev;
+          };
+          tensorflow-datasets = pyprev.tensorflow-datasets.overridePythonAttrs (old: rec {
+            doCheck = false;
+            version = "4.6.0";
+            src = pyprev.fetchPypi {
+              pname = "tensorflow-datasets";
+              inherit version;
+              sha256 = "sha256-naKmUpfpgfYcXxy0VpYGa5xAE9l4A6bfdDgrwk4hN8s=";
+            };
+            pythonImportsCheck = null;
+            propagatedBuildInputs =
+              old.propagatedBuildInputs
+              ++ pyfinal.etils.optional-dependencies.epath
+              ++ [pyfinal.etils];
+          });
+        };
+      in
+        pkgs.python39.override {
+          inherit packageOverrides;
+          self = python;
+        };
+      runtime = p:
         with p; [
           apache-beam
           tensorflow-datasets
           tensorflow
         ];
-      dev-dependencies = p:
+      dev = p:
         with p; [
           ipython
           ipdb
           black
         ];
-      all-dependencies = p: run-dependencies p ++ dev-dependencies p;
+      all = p: runtime p ++ dev p;
     in {
       devShell = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          python39
-          python39.pkgs.tensorflow-datasets
-          python39.pkgs.tensorflow
-        ];
-        #all-dependencies pkgs.python39.pkgs ++
+        buildInputs = all python.pkgs;
         shellHook = ''
           export pythonbreakpoint=ipdb.set_trace
         '';
       };
-      packages.default = pkgs.python39.withPackages all-dependencies;
+      packages.default = python.withPackages all;
     });
 }
