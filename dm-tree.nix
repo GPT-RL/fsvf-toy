fdeb1ebcbd07d5abc0e5165b34566be5180e7f31
Ethan Brooks <ethanabrooks@gmail.com>
2022-07-29 09:06:42 -0400

    fix dm-tree.nix

--- dm-tree.nix
+++ dm-tree.nix
@@ -4,12 +4,14 @@
   pyfinal,
 }:
 pyprev.dm-tree.overridePythonAttrs rec {
+  doCheck = false;
   meta.broken = false;
   pname = "dm-tree";
-  version = " 0.1.7 ";
-  format = "wheel";
+  pythonImportsCheck = [];
+  version = "0.1.7";
+  dontUseSetuptoolsCheck = true;
   src = pyprev.fetchPypi {
-    inherit pname version format;
-    dist = pyfinal.python;
+    inherit pname version;
+    sha256 = "sha256-MP7IrKW5KCPA55ai8zuHW03M1HC1fpHmxUJAXF93/So=";
   };
 }
