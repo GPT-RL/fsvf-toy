75e7d2cb6dee14946d008ff257e88827585a59ff
Ethan Brooks <ethanabrooks@gmail.com>
2022-08-12 16:38:28 -0400

    run black

--- lm1b/train_test.py
+++ lm1b/train_test.py
@@ -25,45 +25,47 @@ from configs import default
 import train
 
 
-jax.config.update('jax_disable_most_optimizations', True)
+jax.config.update("jax_disable_most_optimizations", True)
 
 
 class TrainTest(absltest.TestCase):
-  """Test cases for LM library."""
+    """Test cases for LM library."""
 
-  def setUp(self):
-    super().setUp()
-    tf.config.experimental.set_visible_devices([], 'GPU')
+    def setUp(self):
+        super().setUp()
+        tf.config.experimental.set_visible_devices([], "GPU")
 
-  def test_train_and_evaluate(self):
-    config = default.get_config()
-    config.max_corpus_chars = 1000
-    config.vocab_size = 32
-    config.per_device_batch_size = 2
-    config.num_train_steps = 1
-    config.num_eval_steps = 1
-    config.num_predict_steps = 1
+    def test_train_and_evaluate(self):
+        config = default.get_config()
+        config.max_corpus_chars = 1000
+        config.vocab_size = 32
+        config.per_device_batch_size = 2
+        config.num_train_steps = 1
+        config.num_eval_steps = 1
+        config.num_predict_steps = 1
 
-    config.num_layers = 1
-    config.qkv_dim = 128
-    config.emb_dim = 128
-    config.mlp_dim = 512
-    config.num_heads = 2
+        config.num_layers = 1
+        config.qkv_dim = 128
+        config.emb_dim = 128
+        config.mlp_dim = 512
+        config.num_heads = 2
 
-    config.max_target_length = 32
-    config.max_eval_target_length = 32
-    config.max_predict_length = 32
+        config.max_target_length = 32
+        config.max_eval_target_length = 32
+        config.max_predict_length = 32
 
-    workdir = tempfile.mkdtemp()
+        workdir = tempfile.mkdtemp()
 
-    # Go two directories up to the root of the flax directory.
-    flax_root_dir = pathlib.Path(__file__).parents[2]
-    data_dir = str(flax_root_dir) + '/.tfds/metadata'  # pylint: disable=unused-variable
+        # Go two directories up to the root of the flax directory.
+        flax_root_dir = pathlib.Path(__file__).parents[2]
+        data_dir = (
+            str(flax_root_dir) + "/.tfds/metadata"
+        )  # pylint: disable=unused-variable
 
-    with tfds.testing.mock_data(num_examples=128, data_dir=data_dir):
-      train.train_and_evaluate(config, workdir)
-    logging.info('workdir content: %s', tf.io.gfile.listdir(workdir))
+        with tfds.testing.mock_data(num_examples=128, data_dir=data_dir):
+            train.train_and_evaluate(config, workdir)
+        logging.info("workdir content: %s", tf.io.gfile.listdir(workdir))
 
 
-if __name__ == '__main__':
-  absltest.main()
+if __name__ == "__main__":
+    absltest.main()
