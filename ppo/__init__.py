716a14253f40f0c610e084f3dcf285a4aaff0abf
Ethan Brooks <ethanabrooks@gmail.com>
2022-08-14 13:50:05 -0400

    edit __init__.py

--- ppo/__init__.py
+++ ppo/__init__.py
@@ -1,12 +1,16 @@
 import jaxlib
 import jax
-import gym_minigrid
-import gym
-import tree
+import tensorflow as tf
+# import gym_minigrid
+# import gym
+# import tree
+from pprint import pprint
 
 def main():
-    structure = [[1], [[[2, 3]]], [4]]
-    print(tree.flatten(structure))
+    pprint(tf.config.list_physical_devices())
+    pprint(jax.devices())
+    # structure = [[1], [[[2, 3]]], [4]]
+    # print(tree.flatten(structure))
     # env = gym.make('MiniGrid-Empty-5x5-v0')
     # env.reset()
     # print(env.render("rgb_array"))
