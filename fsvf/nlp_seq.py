a01e94c5dd0dde03b18cca6f07576360045e2e0e
Ethan Brooks <ethanabrooks@gmail.com>
2022-08-25 11:35:47 -0400

    refactor ppo.py to exclude default args from logging

--- fsvf/nlp_seq.py
+++ fsvf/nlp_seq.py
@@ -43,17 +43,6 @@ def param_generator(params: Any):
         yield params
 
 
-def load_config(config_path: Path = CONFIG_PATH) -> dict[str, Any]:
-    with DEFAULTS_PATH.open() as f:
-        config = yaml.load(f, Loader=yaml.FullLoader)
-    if config_path.exists():
-        with config_path.open() as f:
-            config.update(yaml.load(f, Loader=yaml.FullLoader))
-    else:
-        console.log(f"No config file found at {config_path}")
-    return config
-
-
 def no_sweep(**kwargs):
     for params in param_generator(kwargs):
         train(**params)
@@ -64,15 +53,18 @@ def no_sweep(**kwargs):
 def no_log(config_path: Path = CONFIG_PATH):
     assert GRAPHQL_ENDPOINT is not None
     logger = RunLogger(GRAPHQL_ENDPOINT)
-    return no_sweep(logger=logger, **load_config(config_path))
+    with DEFAULTS_PATH.open() as f:
+        with_defaults = yaml.load(f, Loader=yaml.FullLoader)
+    with config_path.open() as f:
+        without_defaults = yaml.load(f, Loader=yaml.FullLoader)
+    with_defaults.update(without_defaults)
+    return no_sweep(logger=logger, **with_defaults)
 
 
 @tree.subcommand(parsers=dict(kwargs=nonpositional(argument("name"))))
 def log(allow_dirty: bool = False, config_path: Path = CONFIG_PATH, **kwargs):
     repo = Repo(".")
-    config = load_config(config_path)
-    config.update(kwargs)
-    return _log(**config, allow_dirty=allow_dirty, repo=repo, sweep_id=None)
+    return _log(**kwargs, allow_dirty=allow_dirty, repo=repo, sweep_id=None)
 
 
 def _log(
@@ -80,7 +72,7 @@ def _log(
     name: str,
     repo: Repo,
     sweep_id: Optional[int],
-    **kwargs,
+    **parameters,
 ):
     if not allow_dirty:
         assert not repo.is_dirty()
@@ -115,10 +107,12 @@ def _log(
     logger = RunLogger(GRAPHQL_ENDPOINT)
     logger.create_run(metadata=metadata, sweep_id=sweep_id, charts=charts)
     logger.update_metadata(  # this updates the metadata stored in the database
-        dict(parameters=kwargs, run_id=logger.run_id, name=name)
+        dict(parameters=parameters, run_id=logger.run_id, name=name)
     )
-
-    (no_sweep if sweep_id is None else train)(**kwargs, logger=logger)
+    with DEFAULTS_PATH.open() as f:
+        with_defaults = yaml.load(f, Loader=yaml.FullLoader)
+    with_defaults.update(parameters)
+    (no_sweep if sweep_id is None else train)(**with_defaults, logger=logger)
 
 
 def trainable(config: dict):
@@ -134,7 +128,8 @@ def sweep(
     random_search: bool = False,
     **kwargs,
 ):
-    partial_config = load_config(config_path)
+    with config_path.open() as f:
+        partial_config = yaml.load(f, Loader=yaml.FullLoader)
     config: "dict[str, Any]" = dict(
         name=name,
         repo=Repo("."),
